6e3be64555d3070ac7a5b8c30a37bbc452e3dbfc
QuintinBot <87077470+QuintinBot@users.noreply.github.com>
2021-07-26 14:58:20 +0200

    init

---
+++ commands/alt.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+const ms = require("ms");
+const timeSpan = ms("200 days");
+
+module.exports.run = async (client, message, args) => {
+
+    if (member.guild.id !== "868620328207798272") return;
+    const createdAt = new Date(member.user.createdAt).getTime();
+    const difference = Date.now() - createdAt;
+
+    if (difference < timeSpan) {
+     member.send("Je hebt een alt account, contact een StaffLid!");
+     member.kick("Alt Account!");
+}
+
+}
+
+
+module.exports.help = {
+    name: "alt"
+}
